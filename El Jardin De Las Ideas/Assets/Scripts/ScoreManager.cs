f309468074eebfb85cb49bd4bf354bd599ac355f
Eduard Casellas <eduardcasellas18@gmail.com>
2018-11-24 03:02:22 +0100

    logic enemy

---
+++ El Jardin De Las Ideas/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+
+	public int score = 0;
+	// Use this for initialization
+	public void addScore (int value) {
+		Debug.Log("addScore");
+		score += value;
+	}
+
+	public void susbtractScore (int value) {
+		Debug.Log("substractScore");
+		score -= value;
+		if (score < 0 ) score = 0;
+	}
+	
+
+
+
+}	
+	
