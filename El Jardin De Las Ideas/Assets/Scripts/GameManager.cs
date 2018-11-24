421a8d3ebe4ec8f55ccc8e2ad373ae580ac15af5
ticidici <ticidici@yahoo.es>
2018-11-24 12:48:17 +0100

    game manager coge todas las flores

--- El Jardin De Las Ideas/Assets/Scripts/GameManager.cs
+++ El Jardin De Las Ideas/Assets/Scripts/GameManager.cs
@@ -1,13 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class GameManager : MonoBehaviour {
 
 	public GameObject scoreManager;
 	public Camera m_Camera;
+
+    private List<FlowerController> flowers;
+    //dos vectores más con las activas e inactivas, con su id, que es la posición como han quedado en la lista
 	
-	// Update is called once per frame
+    void Awake() {
+        //si hubiera más de un tipo de jardín, se tendría que cargar antes
+        flowers = FindObjectsOfType<FlowerController>().ToList();
+        Debug.Log("found flowers: " + flowers.Count);
+    }
+
 	void LateUpdate () {
 		if (Input.GetMouseButtonDown(0))
          {
