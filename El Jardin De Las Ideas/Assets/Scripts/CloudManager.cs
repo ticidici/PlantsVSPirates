6a1bc7e50a3708418d0225d6b6344f9551f96845
Nico <nicolasacostap@gmail.com>
2018-11-24 21:38:10 +0100

    Arreglado el fallo del Input

--- El Jardin De Las Ideas/Assets/Scripts/CloudManager.cs
+++ El Jardin De Las Ideas/Assets/Scripts/CloudManager.cs
@@ -41,9 +41,10 @@ public class CloudManager : MonoBehaviour
                     CompletedWords++;
                     SetAllDropsBlack();
                     Destroy(item.gameObject);
+                    //aux.Remove(item);
+                    drops.Remove(item);
                     InputManager.instance.EmptyBuffer();
                     if(CompletedWords == 1) {
-                        Debug.Log("Bu");
                         CompletedWords = 0;
                         gameManager.makePlantAppear();
                     }
