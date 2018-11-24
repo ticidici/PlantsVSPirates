0ad26d83c41d7071a0d02153d332fb93d3dccdba
Eduard Casellas <eduardcasellas18@gmail.com>
2018-11-24 11:58:20 +0100

    sendMessage droplife

---
+++ El Jardin De Las Ideas/Assets/Scripts/EnemyLogic.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLogic : MonoBehaviour {
+
+	public GameObject flower;
+	private bool active = false;
+	private float timer = 0;
+	public float timeAttack = 2;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		if (active) {
+			timer += Time.deltaTime;
+			if (timer >= timeAttack) {
+				timer = 0;
+				//flower.SendMessage("dropLife");
+			}
+		}
+	}
+}
